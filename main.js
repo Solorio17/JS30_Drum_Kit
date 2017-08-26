447c7a5ca9ef29f8cdd6af68bc6a8f7183a7dfb2
Solorio17 <jasonsoloriom@gmail.com>
2017-08-25 20:03:41 -0600

    Adding javascript functionality

--- main.js
+++ main.js
@@ -1,1 +1,17 @@
+function playSound(d){
+  const audio = document.querySelector(`audio[data-key="${d.keyCode}"]`);
+  const key = document.querySelector(`.key[data-key="${d.keyCode}"]`);
+  if(!audio) return;// stop the function from running all together
+  audio.currentTime =0; // rewinds the audio to beginning, if pressed repeatedly
+  audio.play();
+  key.classList.add("playing");
+}
 
+function removeTransition(d){
+  if(d.propertyName !== "transform") return; //skip if no transform
+  this.classList.remove("playing");
+}
+
+const keys = document.querySelectorAll(".key");
+keys.forEach(key => key.addEventListener("transitionend", removeTransition));
+window.addEventListener("keydown", playSound)
